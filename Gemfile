e9ed0e93351024b09d24dc451f8e0072ad53156a
Kishor Vyavahare <vyavahare.kishor@gmail.com>
2018-02-11 23:47:53 +0530

    Removed scheduler

--- Gemfile
+++ Gemfile
@@ -36,7 +36,6 @@ gem 'jbuilder', '~> 2.5'
 # gem 'capistrano-rails', group: :development
 gem 'bootstrap-sass'
 gem 'sidekiq'
-gem 'sidekiq-scheduler'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   root 'entries#index'
 
   resources :feeds
-  #mount Sidekiq::Web => '/sidekiq'
+  # mount Sidekiq::Web => '/sidekiq'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
